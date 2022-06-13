75a8a38f7cac1d6ea095ec8198f7441d0a029659
catherline nyanduko mirobe <catherline.nyanduko@student.moringaschool.com>
2022-06-13 17:50:26 +0300

    commit tree 13782225306e6c4b2e4583adaadb9707538e9c38

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded",function () {
+    
+    updatetext()
+    console.log("The DOM has loaded");
+});
+function updatetext() {
+    document.getElementById("text").innerHTML="This is really cool!";
+} 
